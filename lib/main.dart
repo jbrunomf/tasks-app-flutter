b771dcbd873b39be2c9a6f852ad37f4552931d81
João Bruno <jbrunomf@outlook.com>
2023-04-02 14:43:39 -0400

    add: Adicionando opacidade para esconder tarefas.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  bool visivel = true;
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -25,21 +27,28 @@ class _MyAppState extends State<MyApp> {
           leading: Container(),
           title: const Text("Minhas tarefas"),
         ),
-        body: ListView(
-          children: const [
-            Task("Teste",
-                "https://www.infoescola.com/wp-content/uploads/2016/08/golfe_235696687-1000x667.jpg", 1),
-            Task("Bike", "", 2),
-            Task("Nadar", "", 3),
-            Task("Nadar", "", 4),
-            Task("Pedalar", "", 5),
-            Task("Pescar",
-                "https://www.infoescola.com/wp-content/uploads/2016/08/golfe_235696687-1000x667.jpg", 5),
-          ],
+        body: AnimatedOpacity(
+          opacity: visivel ? 1 : 0,
+          duration: const Duration(milliseconds: 2000),
+          child: ListView(
+            children: const [
+              Task("Teste",
+                  "https://www.infoescola.com/wp-content/uploads/2016/08/golfe_235696687-1000x667.jpg", 1),
+              Task("Bike", "", 2),
+              Task("Nadar", "", 3),
+              Task("Nadar", "", 4),
+              Task("Pedalar", "", 5),
+              Task("Pescar",
+                  "https://www.infoescola.com/wp-content/uploads/2016/08/golfe_235696687-1000x667.jpg", 5),
+            ],
+          ),
         ),
         floatingActionButton: FloatingActionButton.small(onPressed: () {
-          print("Pressionou o botão");
-        }),
+          setState(() {
+            visivel = !visivel;
+          });
+        },
+        child: const Icon(Icons.remove_red_eye),),
       ),
     );
   }
